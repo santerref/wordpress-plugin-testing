64f6e61db706eea8f649b58d6fc059d04f2dbc9f
Francis Santerre <francis.santerre@gmail.com>
2016-07-23 21:35:09 -0400

    Add Plugin_Singleton unit tests

---
+++ tests/test-plugin-singleton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace wordpress_plugin_testing;
+
+class Plugin_SingletonTest extends \WP_UnitTestCase {
+
+	function test_get_instance() {
+		$instance = Plugin_Singleton::get_instance();
+
+		$this->assertTrue( $instance instanceof Plugin );
+	}
+
+	function test_singleton() {
+		$class = new \ReflectionClass( '\wordpress_plugin_testing\Plugin_Singleton' );
+
+		$this->assertTrue( $class->isFinal() );
+		$this->assertFalse( $class->isCloneable() );
+		$this->assertFalse( $class->isInstantiable() );
+	}
+
+	function test_methods() {
+		$class = new \ReflectionClass( '\wordpress_plugin_testing\Plugin_Singleton' );
+
+		$this->assertTrue( $class->hasMethod( 'get_instance' ) );
+	}
+}
+
